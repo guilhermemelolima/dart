afda2aad00638f6dcaba84b3a610170c4c3a2840
Guilherme Melo de Lima <limaguilherme.melo@gmail.com>
2024-03-06 21:51:47 -0300

    Aula 06/03/2024

---
+++ aula_06_03_2024/ex01/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("Olá mundo!!"),
+        backgroundColor: Colors.greenAccent,
+      ),
+
+      body: _body(),
+
+    );
+  }
+
+  _body() {
+    return ListView(
+      children: [
+        Text("linha1"),
+        Text("linha2"),
+        Text("linha3"),
+        Text("linha4"),
+        Text("linha5")
+      ],
+    );
+  }
+}
---
+++ aula_06_03_2024/ex01/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu primeiro App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
+        useMaterial3: true,
+      ),
+      home: Home()
+    );
+  }
+}
+
