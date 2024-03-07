2e5e8ae6a5ce8c976bf6699ec327c2f233102e11
Guilherme Melo de Lima <limaguilherme.melo@gmail.com>
2024-03-06 21:54:35 -0300

    Aula 06/03/2024

--- aula_06_03_2024/ex01/main.dart
+++ aula_06_03_2024/ex01/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meu primeiro App',
+      title: 'Ex01',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
